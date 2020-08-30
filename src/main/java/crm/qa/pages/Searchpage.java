4fac6c30e02e5a422a53c5baff13f3851e24b531
Your Name <1282vamshi@gmail.com>
2020-08-30 22:20:10 +0530

    added search page and one more test case

---
+++ src/main/java/crm/qa/pages/Searchpage.java
@@ -0,0 +1,9 @@
+package crm.qa.pages;
+
+public class Searchpage {
+
+	public void test()
+	{
+		System.out.println("search page");
+	}
+}
--- src/test/java/crmtests/Testcases.java
+++ src/test/java/crmtests/Testcases.java
@@ -1,5 +1,18 @@
 package crmtests;
 
+import org.testng.annotations.Test;
+
 public class Testcases {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("test case 1");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("test case 2");
+	}
 
 }
